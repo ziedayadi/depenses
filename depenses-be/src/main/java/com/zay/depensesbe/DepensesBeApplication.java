15899cff9e945d0c48c3763a220415b42825f629
mootaz <zied.ayedi22@gmail.com>
2020-03-17 14:04:33 +0100

    INITILELIZE PROJECT

---
+++ depenses-be/src/main/java/com/zay/depensesbe/DepensesBeApplication.java
@@ -0,0 +1,13 @@
+package com.zay.depensesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepensesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepensesBeApplication.class, args);
+	}
+
+}
---
+++ depenses-be/src/test/java/com/zay/depensesbe/DepensesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zay.depensesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepensesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
